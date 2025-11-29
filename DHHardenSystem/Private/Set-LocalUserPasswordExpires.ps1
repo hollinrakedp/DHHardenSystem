7a3d47c42b5c0da193ccc6dff99a9e4a00ab80df
Darren Hollinrake <50473419+hollinrakedp@users.noreply.github.com>
2025-11-29 14:35:17 -0600

    Log when no users need password set to expire

--- DHHardenSystem/Private/Set-LocalUserPasswordExpires.ps1
+++ DHHardenSystem/Private/Set-LocalUserPasswordExpires.ps1
@@ -9,7 +9,7 @@ function Set-LocalUserPasswordExpires {
     .NOTES
     Name       : Set-LocalUserPasswordExpires
     Author     : Darren Hollinrake
-    Version    : 1.1
+    Version    : 1.2
     DateCreated: 2021-08-05
     DateUpdated: 2025-11-29
 
@@ -23,10 +23,15 @@ function Set-LocalUserPasswordExpires {
     )
     $Users = Get-LocalUser | Where-Object { ($_.Enabled -eq $true) -and ($null -eq $_.PasswordExpires) }
 
-    foreach ($User in $Users) {
-        if ($PSCmdlet.ShouldProcess("$User")) {
-            Write-LogEntry -Tee:$Tee -LogMessage "HardenSystem: LocalUserPasswordExpires: User: $User"
-            $User | Set-LocalUser -PasswordNeverExpires:$false
+    if ($Users.Count -eq 0) {
+        Write-LogEntry -Tee:$Tee -LogMessage "HardenSystem: LocalUserPasswordExpires: No users require password expiration update"
+    }
+    else {
+        foreach ($User in $Users) {
+            if ($PSCmdlet.ShouldProcess("$User")) {
+                Write-LogEntry -Tee:$Tee -LogMessage "HardenSystem: LocalUserPasswordExpires: User: $User"
+                $User | Set-LocalUser -PasswordNeverExpires:$false
+            }
         }
     }
 }
